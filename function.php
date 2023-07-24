c3b4d69b4c8a7babc4d9bb5c66fc70c0e4057d12
Restu56 <restuwiliantama@gmail.com>
2023-07-24 16:14:11 +0700

    first commit

---
+++ function.php
@@ -0,0 +1,234 @@
+<?php  
+session_start();
+ 
+//connect to db
+$conn = mysqli_connect("localhost:3307", "root", "", "stokbarang");
+
+// menambah barang baru
+if(isset($_POST['addnewbarang'])){
+
+    $namabarang = $_POST['namabarang'];
+    $jenis = $_POST['jenis'];
+    $stock = $_POST['stock'];
+
+
+    $addtotable = mysqli_query($conn, "insert into stock (namabarang, jenis, stock) 
+                            values('$namabarang', '$jenis', '$stock')");
+    if($addtotable){
+        header('location:index.php');
+    }else{
+        echo 'Gagal';
+        header('location:index.php');
+    }
+};
+
+
+// menambah barang masuk 
+if(isset($_POST['barangmasuk'])){
+    $barangnya = $_POST['barangnya'];
+    $qty = $_POST['qty'];
+    $penerima = $_POST['penerima'];
+ 
+    
+
+    $cekstocksekarang = mysqli_query($conn, "select * from stock where idbarang = '$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstocksekarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang + $qty;
+
+    $addtomasuk   = mysqli_query($conn, "insert into masuk (idbarang, keterangan,qty) values('$barangnya', '$penerima', '$qty')");
+    $updatestockmasuk = mysqli_query($conn, "update stock set stock='$tambahkanstocksekarangdenganquantity' where idbarang='$barangnya'");
+    if($addtomasuk&&$updatestockmasuk){
+        header('location:masuk.php');
+    }else{
+        echo 'Gagal';
+        header('location:masuk.php');
+    }
+
+}
+
+// menambah barang keluar 
+if(isset($_POST['addbarangkeluar'])){
+    $barangnya = $_POST['barangnya'];
+    $qty = $_POST['qty'];
+    $penerima = $_POST['penerima'];
+ 
+    
+
+    $cekstocksekarang = mysqli_query($conn, "select * from stock where idbarang = '$barangnya'");
+    $ambildatanya = mysqli_fetch_array($cekstocksekarang);
+
+    $stocksekarang = $ambildatanya['stock'];
+    $tambahkanstocksekarangdenganquantity = $stocksekarang - $qty;
+
+    $addtokeluar   = mysqli_query($conn, "insert into keluar (idbarang, penerima,qty) values('$barangnya', '$penerima', '$qty')");
+    $updatestockmasuk = mysqli_query($conn, "update stock set stock='$tambahkanstocksekarangdenganquantity' where idbarang='$barangnya'");
+    if($addtokeluar&&$updatestockmasuk){
+        header('location:keluar.php');
+    }else{
+        echo 'Gagal';
+        header('location:keluar.php');
+    }
+
+}
+
+// update info barang
+if(isset($_POST["updatebarang"])){
+    $idb  = $_POST["idb"];
+    $namabarang = $_POST["namabarang"];
+    $jenis = $_POST["jenis"];
+
+    $update = mysqli_query($conn, "update stock set namabarang='$namabarang', jenis='$jenis' where idbarang='$idb'");
+    if($update){
+        header("location:index.php");
+    } else {
+        echo 'Gagal';
+        header("location:index.php");
+    }
+}
+
+// Menghapus barang dari stock
+// if(isset($_POST['hapusbarang'])){
+//     $idb = $_POST['idb'];
+
+//    $hapus = mysqli_query($conn, "delete from stock where idbarang='$idb'"); 
+//     if($hapus){
+//      header("location:index.php");
+//    } else {
+//       echo 'Gagal';
+//      header("location:index.php");
+//    }
+// }
+
+// mengubah barang masuk
+if(isset($_POST{'editbarangmasuk'})){
+    $idb = $_POST['idb'];
+    $idm = $_POST['idm'];
+    $namabarang = $_POST['namabarang'];
+    $qty = $_POST['qty'];
+
+    $lihatstock = mysqli_query($conn, "select * from stovk where idbarang='$idb'");
+    $stocknya = mysqli_fetch_array($lihatstock);
+    $stockskrg = $stock['stock'];
+
+    $qtyskrg = mysqli_query($conn, "select * from masuk where idmasuk='$idm'");
+    $qtynya = mysqli_fetch_array($qtyskrg);
+    $qtyskrg = $qtynya['qty'];
+
+    if($qty>$qtysekarang){
+        $selisih = $qty - $qtyskrg;
+        $kurangin = $stockskrg - $selisih; 
+        $kurangistocknya = mysqli_query($conn, "update stock set stock='$kurangin' where idbarang-'$idb");
+        $updatenya = mysqli_query($conn, "update masuk set qty='$qty',keterangan='$keterangan' where idmasuk='$idm'");
+            if($kurangstocknya&&$updatenya){
+                header('location:masuk.php');
+            }else {
+                echo 'Gagal';
+                header('location:masuk.php');
+            }
+    }else {
+        $selisih = $qtyskrg-$qty;
+        $kurangin = $stockskrg + $selisih;
+        $kurangistocknya = mysqli_query($conn, "update stock set stock='$kurangin' where idbarang='$idb'");
+        $updatenya = mysqli_query($conn, "update masuk set qty='$$qty', keterangan='$keterangan' where idmasuk='$idm'");
+        if($kurangstocknya&&$updatenya){
+            header('location:masuk.php');
+        }else {
+            echo 'Gagal';
+            header('location:masuk.php');
+        }
+    }
+}
+
+// menghapus data barang masuk
+if(isset($_POST['hapusbarangmasuk'])){
+    $idb = $_POST['idb'];
+    $qty = $_POST['kty'];
+    $idm = $_POST['idm'];
+
+    $getdatastock = mysqli_query($conn, "select * from stock where idbarang='$idb'");
+    $data = mysqli_fetch_array($getdatastock);
+    $stock = $data['stock'];
+
+    $selisih = $stock-$qty;
+    
+    $update = mysqli_query($conn, "update stock set stock='$selisih' where idbarang='$idb'"); 
+    $hapusdata = mysqli_query($conn, "delete from masuk where idmasuk='$idm'");
+
+    if($update&&$hapusdata){
+        header('location:masuk.php');
+    }else {
+        header('location:masuk.php');
+    }
+}
+
+//mengubah data barang keluar
+if(isset($_POST{'editbarangmasuk'})){
+    $idb = $_POST['idb'];
+    $idm = $_POST['idk'];
+    $penerima = $_POST['penerima'];
+    $qty = $_POST['qty'];
+
+    $lihatstock = mysqli_query($conn, "select * from stock where idbarang='$idb'");
+    $stocknya = mysqli_fetch_array($lihatstock);
+    $stockskrg = $stock['stock'];
+
+    $qtyskrg = mysqli_query($conn, "select * from keluar where idkeluar='$idk'");
+    $qtynya = mysqli_fetch_array($qtyskrg);
+    $qtyskrg = $qtynya['qty'];
+
+    if($qty>$qtysekarang){
+        $selisih = $qty - $qtyskrg;
+        $kurangin = $stockskrg - $selisih; 
+        $kurangistocknya = mysqli_query($conn, "update stock set stock='$kurangin' where idbarang-'$idb");
+        $updatenya = mysqli_query($conn, "update keluar set qty='$qty',penerima='$penerima' where idkeluar='$idk'");
+            if($kurangstocknya&&$updatenya){
+                header('location:keluar.php');
+            }else {
+                echo 'Gagal';
+                header('location:keluar.php');
+            }
+    }else {
+        $selisih = $qtyskrg-$qty;
+        $kurangin = $stockskrg + $selisih;
+        $kurangistocknya = mysqli_query($conn, "update stock set stock='$kurangin' where idbarang='$idb'");
+        $updatenya = mysqli_query($conn, "update keluar set qty='$$qty', penerima='$penerima' where idkeluar='$idk'");
+        if($kurangstocknya&&$updatenya){
+            header('location:keluar.php');
+        }else {
+            echo 'Gagal';
+            header('location:keluar.php');
+        }
+    }
+}
+
+// menghapus data barang keluar
+if(isset($_POST['hapusbarangkeluar'])){
+    $idb = $_POST['idb'];
+    $qty = $_POST['kty'];
+    $idm = $_POST['idm'];
+
+    $getdatastock = mysqli_query($conn, "select * from stock where idbarang='$idb'");
+    $data = mysqli_fetch_array($getdatastock);
+    $stock = $data['stock'];
+
+    $selisih = $stock-$qty;
+    
+    $update = mysqli_query($conn, "update stock set stock='$selisih' where idbarang='$idb'"); 
+    $hapusdata = mysqli_query($conn, "delete from keluar where idkeluar='$idk'");
+
+    if($update&&$hapusdata){
+        header('location:keluar.php');
+    }else {
+        header('location:keluar.php');
+    }
+}
+
+
+
+; ?>
+
+
+
+
---
+++ stock_opname.php
@@ -0,0 +1,157 @@
+<?php 
+require 'function.php';
+require 'cek.php';
+
+ ; ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Stock Barang</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.html">
+                <img src="gambar/logo.png" alt="" weight="35" height="45">
+            </a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Stock Barang
+                            </a>
+                            <a class="nav-link" href="masuk.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Barang Masuk
+                            </a>  
+                            <a class="nav-link" href="keluar.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Barang Keluar 
+                            </a> 
+                            <a class="nav-link" href="stock_opname.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Stock Opname 
+                            </a> 
+                            <a class="nav-link" href="logout.php">                         
+                                Logout 
+                            </a>                           
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Stock Opname</h1>
+                        <ol class="breadcrumb mb-4">
+                             <li class="breadcrumb-item active">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, voluptatibus.</li>
+                        </ol>
+                        
+                        
+                           
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                                DataTable Example
+                            </div>
+                            <div class="card-body">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                               Tambah Barang
+                            </button>
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            <td>2011/04/25</td>
+                                            <td>$320,800</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Garrett Winters</td>
+                                            <td>Accountant</td>
+                                            <td>Tokyo</td>
+                                            <td>63</td>
+                                            <td>2011/07/25</td>
+                                            <td>$170,750</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Tambah Barang</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal">&times;</button>
+                <br>
+            </div>
+
+            <!-- Modal body -->
+            <form method="post">
+                <div class="modal-body">
+                    <input type="text" name="namabarang" placeholder="Nama Barang" class="form-control" required>
+                    <br>
+                    <input type="text" name="deskripsi" placeholder="Deskripsi Barang" class="form-control" required>
+                    <br>
+                    <input type="number" name="stock"  placeholder="stok" class="form-control" required>
+                    <button type="submit" class="btn btn-primary" name="addnewbarang">Submit</button>
+                </div>
+            </form>
+            </div>
+    </div>
+</html>
